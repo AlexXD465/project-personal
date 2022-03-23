4eb187e97c2d35723185d29f19dd1e49fced0b6c
AlexXD465 <100385240+AlexXD465@users.noreply.github.com>
2022-03-23 09:31:40 -0600

    Add files via upload

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'db_links'
+    }
+
+};
